afa70b4b2e7b7d366a8af21ea96fa709b6e58c31
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-02-26 21:47:26 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -31,10 +31,8 @@ CREATE TABLE characters (
     series_id    INTEGER
 );
 
-CREATE TABLE character_books
+CREATE TABLE character_books (
     id           INTEGER PRIMARY KEY,
     book_id      INTEGER,
     character_id INTEGER
 );
-
-
